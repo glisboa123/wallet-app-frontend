0b13450419cd8b97d19be1a1858303996d96cd84
Giuliano Rodrigues <giulianorodrigues2018@gmail.com>
2023-04-15 23:34:11 -0300

    feat: implement render data on table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,61 @@
+const renderFinanceList = (data) => {
+  const table = document.getElementById("finances-table");
+  // <tr>
+  //   <td>Item 1</td>
+  //   <td>Item 2</td>
+  //   <td>Item 3</td>
+  //   <td class="center">Item 3</td>
+  //   <td class="right">Item 3</td>
+  // </tr>;
+
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    // title
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    // category
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    // category
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(
+      new Date(item.date).toLocaleDateString()
+    );
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    // value
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    // delete
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    // table add table row
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinanceElements = (data) => {
   const totalItems = data.length;
   const revenues = data
@@ -77,6 +135,7 @@ const onLoadFinancesData = async () => {
     );
     const data = await result.json();
     renderFinanceElements(data);
+    renderFinanceList(data);
     console.log(data);
     return data;
   } catch (error) {
