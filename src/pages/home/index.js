ead5317db4d2a0c445f427677173e29f5d74851a
Giuliano Rodrigues <giulianorodrigues2018@gmail.com>
2023-04-15 22:45:56 -0300

    feat: implement finance cards

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,89 @@
+const renderFinanceElements = (data) => {
+  const totalItems = data.length;
+  const revenues = data
+    .filter((item) => Number(item.value) > 0)
+    .reduce((acc, item) => acc + Number(item.value), 0);
+  const expenses = data
+    .filter((item) => Number(item.value) < 0)
+    .reduce((acc, item) => acc + Number(item.value), 0);
+  const totalValue = Number(revenues) + Number(expenses);
+  const totalValueFormated = totalValue;
+
+  console.log(totalValue);
+
+  // render total releases
+  const financeCard1 = document.getElementById("finance-card-1");
+  const totalReleases = document.createTextNode(totalItems);
+  const totalReleasesElement = document.createElement("h1");
+  totalReleasesElement.className = "mt smaller";
+  totalReleasesElement.appendChild(totalReleases);
+  financeCard1.appendChild(totalReleasesElement);
+
+  // render total revenue
+  const financeCard2 = document.getElementById("finance-card-2");
+  const revenueText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(`${revenues}`)
+  );
+  const revenueTextElement = document.createElement("h1");
+  revenueTextElement.className = "mt smaller";
+  revenueTextElement.style.color = "#57ce42";
+  revenueTextElement.appendChild(revenueText);
+  financeCard2.appendChild(revenueTextElement);
+
+  // render total expenses
+  const financeCard3 = document.getElementById("finance-card-3");
+  const expensesText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(`${expenses * -1}`)
+  );
+  const expensesTextElement = document.createElement("h1");
+  expensesTextElement.className = "mt smaller";
+  expensesTextElement.style.color = "#ee2126";
+  expensesTextElement.appendChild(expensesText);
+  financeCard3.appendChild(expensesTextElement);
+
+  // render total balance
+  const financeCard4 = document.getElementById("finance-card-4");
+  const balanceText = document.createTextNode(
+    new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(`${totalValueFormated}`)
+  );
+  const balanceTextElement = document.createElement("h1");
+  balanceTextElement.className = "mt smaller";
+  balanceTextElement.style.color = "#5936cd";
+  balanceTextElement.appendChild(balanceText);
+  financeCard4.appendChild(balanceTextElement);
+};
+
+const onLoadFinancesData = async () => {
+  try {
+    const email = localStorage.getItem("@WalletApp:userEmail");
+    const date = "2022-12-15";
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/finances?date=${date}`,
+      {
+        method: "GET",
+        headers: {
+          email: email,
+        },
+      }
+    );
+    const data = await result.json();
+    renderFinanceElements(data);
+    console.log(data);
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
+
 const onLoadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
   const name = localStorage.getItem("@WalletApp:userName");
@@ -26,4 +112,5 @@ const onLoadUserInfo = () => {
 
 window.onload = () => {
   onLoadUserInfo();
+  onLoadFinancesData();
 };
